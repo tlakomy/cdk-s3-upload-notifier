e8a92061c8bed0086e98c75b9ffaf702a247217f
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-11-13 17:46:38 +0100

    Added s3-upload-notifier

--- lib/s3-upload-notifier-stack.ts
+++ lib/s3-upload-notifier-stack.ts
@@ -1,9 +1,27 @@
 import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as sns from '@aws-cdk/aws-sns';
+import * as s3Notifications from '@aws-cdk/aws-s3-notifications';
+import * as snsSubscriptions from '@aws-cdk/aws-sns-subscriptions';
 
 export class S3UploadNotifierStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        const dataBucket = new s3.Bucket(this, 'DataBucket');
+        const topic = new sns.Topic(this, 'Topic');
+
+        dataBucket.addEventNotification(
+            s3.EventType.OBJECT_CREATED,
+            new s3Notifications.SnsDestination(topic),
+        );
+
+        const emailAddress = new cdk.CfnParameter(this, 'subscriptionEmail');
+
+        topic.addSubscription(
+            new snsSubscriptions.EmailSubscription(
+                emailAddress.value.toString(),
+            ),
+        );
+    }
 }
