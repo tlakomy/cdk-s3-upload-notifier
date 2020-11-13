4194eb25661207cad4f54e0db0eb5f7bb675a7e5
Tomasz Łakomy <tomasz.lakomy@olx.pl>
2020-11-13 18:25:47 +0100

    Added template.yml

--- lib/s3-upload-notifier-stack.ts
+++ lib/s3-upload-notifier-stack.ts
@@ -8,18 +8,22 @@ export class S3UploadNotifierStack extends cdk.Stack {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
+        // We're going to leave everything by default, but you might want to enable versioning on the bucket
         const dataBucket = new s3.Bucket(this, 'DataBucket');
         const topic = new sns.Topic(this, 'Topic');
 
+        // Whenever there's an OBJECT_CREATED event, send a notification to SNS topic
         dataBucket.addEventNotification(
             s3.EventType.OBJECT_CREATED,
             new s3Notifications.SnsDestination(topic),
         );
 
+        // This value is provided as a CloudFormation parameter
         const emailAddress = new cdk.CfnParameter(this, 'subscriptionEmail');
 
         topic.addSubscription(
             new snsSubscriptions.EmailSubscription(
+                // Since we're using TypeScript, we will immediately know if we've forgot to convert the `value` to a `string`
                 emailAddress.value.toString(),
             ),
         );
